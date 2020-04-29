6ffd70b23856dfbd2c8696f07ebb9d5a948b62ca
Philipp Scholz <pscholz@outlook.de>
2020-04-29 17:12:03 +0200

    Issue #40

--- include/LevelDensity/LevelDensity.h
+++ include/LevelDensity/LevelDensity.h
@@ -1,25 +1,66 @@
+/**
+ * @file LevelDensity.h
+ * @date 2020-04-29
+ * @brief Declaration and definition of LevelDensity class and it's members
+ */
 #pragma once
 #include <string>
 
+/**
+ * @brief Base class for all LevelDensity models.
+ * @details
+ * Important are 
+ * - TotalLevelDensity()
+ * - CalculateDensity()
+ * 
+ * These methods should be provided by all other NLD models.
+ */
 class LevelDensity {
  public:
- LevelDensity(int Z, int A, double J, int parity) :
-  Z_(Z), A_(A), J_(J), parity_(parity) {
+ /**
+  * @brief Constuctor of a LevelDensity.
+  * @param Z Charge number
+  * @param A Mass number
+  * @param J Spin
+  * @param P Parity
+  */
+ LevelDensity(int Z, int A, double J, int P) :
+  Z_(Z), A_(A), J_(J), parity_(P) {
   };
+
+  /**
+   * @brief Destructor
+   */
   virtual ~LevelDensity(){};
+
+  /**
+   * @brief Operator which is used sometimes in the code to call CalculateDensity().
+   */
   double operator()(double E){
     return CalculateDensity(E);
   };
   
+  /**
+   * @brief For some reasons, these classes need to have access to some functions.
+   */
   friend class KopeckyUhlGSF;
   friend class TransitionRateFunc;
 
  protected:
-  
-  
+  /**
+   * @brief Method to set tables
+   */ 
   void SetTables(bool x){tables = x;};
 
+  /**
+   * @brief Method to calculate the total Level Density.
+   */
   virtual double TotalLevelDensity(double E){};
+
+  /**
+   * @brief Method to calculate the partial density.
+   * @param E Excitation Energy. 
+   */
   virtual double CalculateDensity(double E){};
 
   
@@ -30,7 +71,7 @@ class LevelDensity {
   int A_; /**< Mass number of the nucleus*/
   int parity_; /**< Parity of the states*/
   double J_; /**< Spin of the states*/
-  bool tables = false;
+  bool tables = false; /**< Boolean to decide if its a LevelDensityTable or Formular.*/
 };
 
 
