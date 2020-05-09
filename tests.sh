dd721fe59652a81d76d6028069642b1fe0027cab
phscholz <pscholz@outlook.de>
2020-05-09 17:06:35 +0200

    exception to tests.sh script

--- tests.sh
+++ tests.sh
@@ -6,13 +6,13 @@
 ## @brief Script to test Sapphire
 ######################################################
 
-build/sapphire
-build/sapphire reaction
-build/sapphire decayer
-build/sapphire random 
-build/sapphire old
-build/sapphire template
-build/sapphire help
-build/sapphire reaction examples/test_reaction.ini
-build/sapphire decayer examples/test_decayer.ini
-build/sapphire decayer examples/test_random.ini
+build/sapphire || echo "Sapphire test failed!"
+build/sapphire reaction || echo "Sapphire reaction test failed!"
+build/sapphire decayer || echo "Sapphire decayer test failed!"
+build/sapphire random || echo "Sapphire random test failed!"
+build/sapphire old || echo "Sapphire old test failed!"
+build/sapphire template || echo "Sapphire template test failed!"
+build/sapphire help || echo "Sapphire help test failed!"
+build/sapphire reaction examples/test_reaction.ini || echo "Sapphire reaction examples/test_reaction.ini failed!"
+build/sapphire decayer examples/test_decayer.ini || echo "Sapphire decayer examples/test_decayer.ini failed!"
+build/sapphire random examples/test_random.ini || echo "Sapphire random examples/test_random.ini failed!"
