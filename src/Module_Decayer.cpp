1a35b809da17666d6c0e4d3e3b5235fe269d8e95
phscholz <pscholz@outlook.de>
2020-05-10 18:30:55 +0200

    changed omp_set_num_threads()

--- src/Module_Decayer.cpp
+++ src/Module_Decayer.cpp
@@ -143,7 +143,7 @@ namespace Module_Decayer{
         *   is fixed at 10 to prevent the "segmentation fault" bug. 
         *   Once this issue is fixed, this can be removed.
         */
-        if(omp_get_max_threads() > 10) omp_set_num_threads(10);
+        if(omp_get_max_threads() > 10) omp_set_num_threads(18);
 
         /**
         *   3. In a next step, the input parameters for the Decayer are printed to std::ccout.
